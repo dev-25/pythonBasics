2cd5c4b9da171d1a02489f1004f46245cccfcaf3
Devesh <dev250701@gmail.com>
2023-08-03 00:45:59 +0530

    Basics

---
+++ OOPS/abstraction.py
@@ -0,0 +1,32 @@
+
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+def main():
+    square = Square(5)
+    print(square.area())
+
+    circle = Circle(3)
+    print(circle.area())
+
+if __name__ == "__main__":
+    main()
